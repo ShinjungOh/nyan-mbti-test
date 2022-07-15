62682fb1cc0367493dae2d935d8ec7a1d260477e
ShinjungOh <tlswjd9260@gmail.com>
2022-07-15 17:14:46 +0900

    [ADD] react router 설정

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import Home from "./pages/Home";
 import Question from "./pages/Question";
 import Result from "./pages/Result";
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-        <Home/>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/question' element={<Question/>}/>
+                <Route path='/result' element={<Result/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
